55b52594ffcaf90ff94074a2aae4b243011c570f
D3TRU04 <danieltruong1007@gmail.com>
2025-04-19 15:29:01 -0500

    Initial commit: Downbad.ai - Universal Profile Analysis Platform

---
+++ dist/basic-exports.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.program = exports.PROGRAM_ID = void 0;
+// Basic exports for the anchor program
+exports.PROGRAM_ID = 'your_program_id_here';
+exports.program = {
+// Add your program implementation here
+};
---
+++ src/basic-exports.ts
@@ -0,0 +1,10 @@
+// Basic exports for the anchor program
+export const PROGRAM_ID = 'your_program_id_here';
+
+export interface BasicProgram {
+  // Add your program interface here
+}
+
+export const program: BasicProgram = {
+  // Add your program implementation here
+}; 
