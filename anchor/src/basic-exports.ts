1501784a2434db9e7fb5b472763079ebd068c7ef
D3TRU04 <danieltruong1007@gmail.com>
2025-06-11 22:03:37 -0500

    Final changes

--- anchor/src/basic-exports.ts
+++ anchor/src/basic-exports.ts
@@ -1,5 +1,5 @@
 // Here we export some useful types and functions for interacting with the Anchor program.
-import { AnchorProvider, Program } from '@coral-xyz/anchor'
+import { AnchorProvider, Program, Idl } from '@coral-xyz/anchor'
 import { PublicKey } from '@solana/web3.js'
 import BasicIDL from '../target/idl/basic.json'
 import type { Basic } from '../target/types/basic'
@@ -12,5 +12,5 @@ export const BASIC_PROGRAM_ID = new PublicKey(BasicIDL.address)
 
 // This is a helper function to get the Basic Anchor program.
 export function getBasicProgram(provider: AnchorProvider) {
-  return new Program(BasicIDL as Basic, provider)
+  return new Program(BasicIDL as Idl, BASIC_PROGRAM_ID, provider)
 }
