9fe13b81f80dce7fdc4c879f7e1cb29bc18aab9c
D3TRU04 <danieltruong1007@gmail.com>
2025-06-15 15:17:25 -0500

    Testing Vercel

---
+++ anchor/target/types/basic.ts
@@ -0,0 +1,33 @@
+export type Basic = {
+  "version": "0.1.0",
+  "name": "basic",
+  "instructions": [
+    {
+      "name": "greet",
+      "accounts": [],
+      "args": []
+    }
+  ],
+  "accounts": [],
+  "events": [],
+  "errors": [],
+  "types": [],
+  "constants": []
+};
+
+export const IDL: Basic = {
+  "version": "0.1.0",
+  "name": "basic",
+  "instructions": [
+    {
+      "name": "greet",
+      "accounts": [],
+      "args": []
+    }
+  ],
+  "accounts": [],
+  "events": [],
+  "errors": [],
+  "types": [],
+  "constants": []
+};
